b49a251e398b26c24903f37c3c63018b414d4449
mauperez9918 <mauperez9918@gmail.com>
2023-05-30 19:09:56 -0300

    Cambios en la forma de ingresar los comandos

--- js/index.js
+++ js/index.js
@@ -5,12 +5,12 @@ let usuario = "";
 let claveUsuario = ""
 
 function consola() {
-    let nombre = prompt("Cual es su nombre?");
-    comando = prompt("\n-Ingresar a mi cuenta\n-Ver productos\n-Contactar al soporte\n-Salir");
+    let nombre = prompt("Bienvenido cual es tu nombre?");
+    comando = prompt("Ingrese el numero de la opcion que desea:\n1- Ingresar a mi cuenta\n2- Ver productos\n3- Contactar al soporte\n4- Salir");
 
     while (comando != "" || comando != "Salir") {
         switch (comando) {
-            case "Ingresar a mi cuenta":
+            case "1":
                 usuario = prompt("Ingresar su usuario.");
                 if (USUARIO == usuario) {
                     claveUsuario = Number(prompt("Ingrese su clave."));
@@ -27,13 +27,13 @@ function consola() {
                 }
                 consola()
                 break;
-            case "Ver productos":
+            case "2":
                 for (let producto = 0; producto <= 5; producto++) {
                     alert ("Este es el producto Nro " + producto);
                 }
                 consola()
                 break;
-            case "Contactar al soporte":
+            case "3":
                 let mensaje = prompt("Escriba el mensaje que desea enviar al soporte.");
                 if (mensaje != "") {
                     alert("El mensaje ha sido enviado con exito.");
@@ -42,7 +42,7 @@ function consola() {
                 }
                 consola()
                 break;
-                case "Salir":
+                case "4":
                     alert ("Gracias por utilizar la consola.");
                     break;
             default:
