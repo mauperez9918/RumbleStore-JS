92b6f78f26e0179a6b86e48325a8ad5327536c0f
mauperez9918 <mauperez9918@gmail.com>
2023-07-23 23:58:18 -0300

    buscador, categorias y librerias

--- js/index.js
+++ js/index.js
@@ -1,79 +1,23 @@
-const productos = [
-  {
-    id: 1,
-    nombre: "Mother Asus ROG Maximus Z690 Formula LGA1700",
-    precio: 1290,
-    imagen:
-      "https://mauperez9918.github.io/PFMauricioPerez/images/index/asus-z690.jpg",
-  },
-  {
-    id: 2,
-    nombre: "AMD Ryzen 9 7950X AM5",
-    precio: 1000,
-    imagen:
-      "https://mauperez9918.github.io/PFMauricioPerez/images/index/ryzen9-7950x.jpg",
-  },
-  {
-    id: 3,
-    nombre: "CPU Intel Core i9 13900K Raptor Lake 1700",
-    precio: 929,
-    imagen:
-      "https://mauperez9918.github.io/PFMauricioPerez/images/index/i9-13900k.jpg",
-  },
-  {
-    id: 4,
-    nombre: "GPU Zotac GeForce RTX 4090 Trinity 24Gb",
-    precio: 2700,
-    imagen:
-      "https://mauperez9918.github.io/PFMauricioPerez/images/index/zotac-geforce-4090.jpg",
-  },
-  {
-    id: 5,
-    nombre: "AORUS SSD PCIe Gen4 7000s 1TB",
-    precio: 360,
-    imagen:
-      "https://mauperez9918.github.io/PFMauricioPerez/images/index/aorus-ssd-1tb.jpg",
-  },
-  {
-    id: 6,
-    nombre: "Memoria Adata XPG Lancer RGB 16Gb DDR5 6000Mhz White",
-    precio: 164,
-    imagen:
-      "https://mauperez9918.github.io/PFMauricioPerez/images/index/adata-xpg-ddr5.jpg",
-  },
-  {
-    id: 7,
-    nombre: "Monitor ASUS TUF VG27VH1B Curvo 165hz",
-    precio: 399,
-    imagen:
-      "https://mauperez9918.github.io/PFMauricioPerez/images/index/astuf-monitor.jpg",
-  },
-  {
-    id: 8,
-    nombre: "Notebook AERO i9-13900H / 32GB DDR5 / 1TB / 16″ 4K OLED",
-    precio: 3740,
-    imagen:
-      "https://mauperez9918.github.io/PFMauricioPerez/images/index/aero16-laptop.webp",
-  },
-  {
-    id: 9,
-    nombre: "Teclado HyperX Alloy Origins 60% Aqua Tactile",
-    precio: 110,
-    imagen:
-      "https://mauperez9918.github.io/PFMauricioPerez/images/index/hyperx-alloy-origins.jpg",
-  },
-  {
-    id: 10,
-    nombre: "Fuente ASUS ROG STRIX 850G Gold",
-    precio: 250,
-    imagen:
-      "https://mauperez9918.github.io/PFMauricioPerez/images/index/fuente-asus-850watts.png",
-  },
-];
+let productos = [];
+let productosPorCategoria = [];
+let listaDeNovedades = [];
+let productosFiltrados = [];
 
-function listadoDeProductos() {
+async function cargarNovedades() {
+  const response = await fetch(`../novedades.json`);
+  listaDeNovedades = await response.json();
+  listadoDeProductos(listaDeNovedades);
+}
+
+async function cargarProductos() {
+  const response = await fetch(`../productos.json`);
+  productos = await response.json();
+}
+
+function listadoDeProductos(array) {
   let novedades = document.getElementById("novedades");
-  productos.forEach((producto) => {
+  novedades.innerHTML = "";
+  array.forEach((producto) => {
     novedades.innerHTML += `<article class='tarjeta'>
     <img src='${producto.imagen}'/>
     <h3>${producto.nombre}</h3>
@@ -93,7 +37,8 @@ function listadoDeProductos() {
   }
 }
 
-listadoDeProductos();
+cargarNovedades();
+cargarProductos();
 
 function agregarProducto(id) {
   const carritoStorage = JSON.parse(localStorage.getItem("carrito")) || [];
@@ -109,5 +54,38 @@ function agregarProducto(id) {
     const productoCarrito = productos.find((producto) => producto.id === id);
     carritoStorage.push({ ...productoCarrito, cantidad: 1 });
     localStorage.setItem("carrito", JSON.stringify(carritoStorage));
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      text: "Tu producto ha sido agregado al carrito.",
+      showConfirmButton: false,
+    });
   }
+}
+
+const botonesCategorias = document.querySelectorAll(".btnCategorias");
+botonesCategorias.forEach((boton) => {
+  boton.addEventListener("click", (event) => {
+    event.preventDefault();
+    productosPorCategoria = filtrarCategorias(boton.dataset.cat);
+    document.getElementById("titleindex").innerText = boton.dataset.cat;
+    listadoDeProductos(productosPorCategoria);
+  });
+});
+
+function filtrarCategorias(categoria) {
+  return productos.filter((producto) => producto.categoria == categoria);
+}
+
+const buscador = document.getElementById("buscador");
+buscador.addEventListener("keydown", (event) => {
+  event.preventDefault();
+  productosFiltrados = filtrarPorNombre(buscador.value);
+  listadoDeProductos(productosFiltrados);
+});
+
+function filtrarPorNombre(palabra) {
+  return productos.filter((producto) =>
+    producto.nombre.toLowercase().includes(palabra)
+  );
 }
